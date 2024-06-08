1a534b93ea46b61af489bdb5d117a60d1cb4e928
lelouch187 <vanushs187@gmail.com>
2024-06-08 22:38:04 +0600

    read inputs value in loginform

--- src/components/loginForm/LoginForm.tsx
+++ src/components/loginForm/LoginForm.tsx
@@ -1,25 +1,38 @@
 import MyInput from "../ui/MyInput.tsx";
 import MyBtn from "../ui/MyBtn/MyBtn.tsx";
+import {SubmitHandler, useForm} from "react-hook-form";
 
+type Inputs = {
+    email: string;
+    password: string;
+    rememberMe?: boolean;
+    captcha: boolean;
+}
 
 
 const LoginForm = () => {
-
+    const {
+        register,
+        handleSubmit
+    } = useForm<Inputs>()
+    const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
 
     return (
-        <form className="flex flex-col items-center justify-center">
+        <form onSubmit={handleSubmit(onSubmit)}
+              className="flex flex-col items-center justify-center">
             <label className='w-full'>Введите email:
-                <MyInput/>
+                <MyInput type="email" {...register("email")}/>
             </label>
             <label className='w-full'>Введите пароль:
-                <MyInput/>
+                <MyInput type="password" {...register("password")}/>
             </label>
             <MyBtn className={'px-4 py-2 w-20 h-10 bg-blue-600 text-white rounded-lg hover:bg-blue-700'}>
                 Войти
             </MyBtn>
             <label className='mt-4'>запомнить меня?
-                <input type='checkbox'
-                className='ml-2'
+                <input {...register("rememberMe")}
+                    type='checkbox'
+                       className='ml-2'
                 />
             </label>
         </form>
--- src/components/ui/MyInput.tsx
+++ src/components/ui/MyInput.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, {ForwardedRef, forwardRef} from 'react';
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
     error?: string;
     success?: string;
 }
 
-const MyInput = (props: InputProps) => {
+const MyInput =forwardRef((props: InputProps, ref:ForwardedRef<HTMLInputElement>) => {
     let styles = 'px-2 py-2 mb-4 rounded-lg outline-none w-full border-[2px] '
     if (props.error) {
         styles += 'border-red-600 '
@@ -16,9 +16,10 @@ const MyInput = (props: InputProps) => {
 
     return (
         <input {...props}
+               ref={ref}
                className={styles}
         />
     );
-};
+})
 
 export default MyInput;
